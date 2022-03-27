02ea974b3390ca8c0f18a94d347ace60b66064a2
renshimosawa <polaris2760@gmail.com>
2022-03-27 12:29:24 +0900

    addstyle

--- pages/login.tsx
+++ pages/login.tsx
@@ -15,28 +15,42 @@ const LoginPage: NextPage = () => {
     router.push('./dashboard')
   }
   return (
-    <div>
-      <h1>ログイン</h1>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-
-          <input id="email" value={email} onInput={(e) => setEmail(e.currentTarget.value)} />
+    <div className="flex justify-center items-center h-screen">
+      <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div className="card-body" onSubmit={onSubmit}>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="email"
+              className="input input-bordered"
+              id="email"
+              value={email}
+              onInput={(e) => setEmail(e.currentTarget.value)}
+            ></input>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              placeholder="password"
+              className="input input-bordered"
+              id="password"
+              type="password"
+              value={password}
+              onInput={(e) => setPassword(e.currentTarget.value)}
+            ></input>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary" type="submit">
+              Login
+            </button>
+          </div>
         </div>
-
-        <div>
-          <label htmlFor="password">Password:</label>
-
-          <input
-            id="password"
-            type="password"
-            value={password}
-            onInput={(e) => setPassword(e.currentTarget.value)}
-          />
-        </div>
-
-        <button type="submit">login</button>
-      </form>
+      </div>
     </div>
   )
 }
